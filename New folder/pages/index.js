06979a0a79717a40e2446b5cc3e0d852b7b716fb
GAURAV-KUMAR22 <gorav.panwar99@gmail.com>
2024-10-22 14:38:39 +0530

    deploye project

---
+++ New folder/pages/index.js
@@ -0,0 +1,43 @@
+import { MongoClient } from 'mongodb';
+import MeetupList from '../components/meetups/MeetupList';
+import { Fragment } from 'react';
+import Head from 'next/head';
+
+
+
+function HomePage(props) {
+    return (
+        <Fragment>
+            <Head>
+                <title>React Metups</title>
+                <meta name='description' content='Browse a huge list of highly active react meetups' />
+            </Head>
+            <MeetupList meetups={props.meetups} />;
+        </Fragment>)
+}
+
+export async function getStaticProps() {
+    // fetch data from an API
+    const client = await MongoClient.connect(
+        'mongodb+srv://choudharygaurav884:GZa1ZkXc4eVvtiYE@nextjs.4w2dl.mongodb.net/?retryWrites=true&w=majority&appName=NEXTJS'
+    )
+    const db = client.db()
+
+    const meetupsCollection = db.collection('meetups');
+
+    const meetups = await meetupsCollection.find().toArray();
+    client.close();
+    return {
+        props: {
+            meetups: meetups.map(meetup => ({
+                title: meetup.title,
+                address: meetup.address,
+                image: meetup.image,
+                id: meetup._id.toString()
+            }))
+        },
+        revalidate: 1
+    };
+}
+
+export default HomePage;
